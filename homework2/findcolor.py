f94026d54227f7aa0eb91275eac3fbfd89cc8320
Geoffrey Ulman <ulman@metsci.com>
2012-02-04 17:14:23 -0500

    homework2: cleaned up code.

--- homework2/findcolor.py
+++ homework2/findcolor.py
@@ -1,7 +1,12 @@
 import os, sys, scipy, numpy, Image, sophia
 
-def findcolor( image, color, tolerance ):
-  """Given an image (or a file path pointing to an image file) and a size three tuple representing an RGB color returns a new image with all pixels not near the given color masked in black."""
+def main():
+  findcolor( findfile( '20120129a.jpg' ) ).show()
+
+def findcolor( image, color=(255,131,22), tolerance=30, mode=1 ):
+  """
+Given an image (or a file path pointing to an image file) and a size three tuple representing an RGB color returns a new image with all pixels not near the given color masked in black.
+mode = 1 returns a black and white mask, mode = 0 returns the mask applied to the original image."""
  
   # if image is a string, load the image it points to, otherwise
   # assume that it already is an image
@@ -38,7 +43,14 @@ def findcolor( image, color, tolerance ):
   bi_masked = sophia.a2if( ba_masked )
 
   # combine the masked channels into a single RGB image
-  final_image = Image.merge( 'RGB', ( ri_masked, gi_masked, bi_masked ) )
+  if mode:
+    final_image = sophia.a2if( overall_mask * 255 )
+  else:
+    # create images from the three masked channels
+    ri_masked = sophia.a2if( ra_masked )
+    gi_masked = sophia.a2if( ga_masked )
+    bi_masked = sophia.a2if( ba_masked )
+    final_image = Image.merge( 'RGB', ( ri_masked, gi_masked, bi_masked ) )
 
   return final_image
 
@@ -48,4 +60,14 @@ def mask( array, value, tolerance ):
   array_low  = ( array > float(value - tolerance) )
   array_high = ( array < float(value + tolerance) )
   return array_low * array_high
- 
+
+# inspired by: http://dotnot.org/blog/archives/2004/03/06/find-a-file-in-pythons-path/
+# I wanted a way to automatically find the named image file in the same way python finds imported modules (similar to seaching for resources on the Java classpath)
+def findfile( name ):
+  for directory in sys.path:
+    potential = os.path.join( directory, name )
+    if os.path.isfile( potential ):
+      return potential
+
+if __name__ == "__main__":
+  main() 

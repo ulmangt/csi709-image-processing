93a20c7631fe17d9c97f5b09789cd7f9b71f1da1
Geoffrey Ulman <ulman@metsci.com>
2012-02-26 11:59:59 -0500

    homework 3

--- start.py
+++ start.py
@@ -3,8 +3,8 @@ os.chdir( '/home/ulman/CSI709/csi709-image-processing/' )
 sys.path.append( 'lib' )
 import scipy, numpy, Image, sophia
 
-sys.path.append( 'homework3' )
-import geoff_ulman_homework3
+sys.path.append( 'homework4' )
+import geoff_ulman_homework4 as hw
 
-evls, emgs = geoff_ulman_homework3.generateEigenImagesDirectory( '/home/ulman/CSI709/csi709-image-processing/test-smaller' )
+
 

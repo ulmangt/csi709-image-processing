6d9b458e35561058e0c6b297b4a3e6650176614f
Geoffrey Ulman <ulman@metsci.com>
2012-04-10 23:57:05 -0400

    homework 5

--- homework5/geoff_ulman_homework5.py
+++ homework5/geoff_ulman_homework5.py
@@ -1,41 +1,88 @@
 import os, sys, scipy, numpy, Image, sophia, color, fpf
 from scipy.fftpack import fft2, ifft2
+from scipy.signal import convolve2d
+from scipy.ndimage.measurements import label
+from scipy import zeros, ones
 
-def main():
-  return findBoats( "boatsmall.jpg", 0, 18, "boat", "png" )
+def main_allboats( fp=0 ):
+  """
+  Train to detect all 18 boat images and provide two clutter images which are
+  trained against (images of the shiny peaks on the docks).
+  """
+  boats = loadImages( 18, "boat", "png" )
+  clutter = loadImages( 2, "clutter", "png" )
+  return findBoats( fp, "boatsmall.jpg", boats, clutter )
 
-def findBoats( image_name, fp, num_train, prefix_train, extension_train ):
+
+def main_missingboat( fp=0 ):
+  """
+  Train to detect 16 of the boat images and train against the two large
+  boat images on the southern portion of the dock.
+  """
+  boats = loadImages( 16, "boat", "png" )
+  clutter = [ loadMatrix( "boat16.png" ), loadMatrix( "boat17.png" ) ]
+  return findBoats( fp, "boatsmall.jpg", boats, clutter )
+
+
+def findBoats( fp, image_name, boats, clutter ):
+
+  # load the full boats image
   full_image = loadMatrix( image_name )
-  boats = loadBoats( num_train, prefix_train, extension_train )
   
+  # get the dimensions of the boat image
   rows = full_image.shape[0]
   cols = full_image.shape[1]
 
-  mat_boats = numpy.zeros( ( num_train, rows * cols ) )
-  centered_boats = []
-  for i,boat in enumerate( boats ):
-    centered_boat = sophia.Plop( boat, rows, cols )
-    centered_boats.append( centered_boat )
+  # combine the boats ( constraint=1 )
+  # and the clutter ( constraint=0 ) images 
+  images = []
+  images.extend( boats )
+  images.extend( clutter )
+
+  # construct a matrix with one rwo for each image
+  mat_images = numpy.zeros( ( len( images ), rows * cols ), complex )
+  centered_images = []
+
+  # loop through each image, centering it in a frame the
+  # size of the originial boats image
+  # then perform a 2d fft and place the raveled image
+  # into a row of the matrix
+  for i,image in enumerate( images ):
+    centered_image = sophia.Plop( image, rows, cols )
+    centered_images.append( centered_image )
     if fp == 0:
-      mat_boats[i,:] = centered_boat.ravel( )
+      mat_images[i,:] = centered_image.ravel( )
     else:
-      mat_boats[i,:] = fft2( centered_boat ).ravel( )
+      mat_images[i,:] = fft2( centered_image ).ravel( )
 
-  constraint = numpy.ones( num_train )
+  # build the constraint vector
+  constraint = numpy.append( numpy.ones( len( boats ) ), numpy.zeros( len( clutter ) ) )
 
-
+  # use FPF to build the fractional power filter which will
+  # be correlated with the original image
   if fp == 0:
-    filt = fpf.FPF( mat_boats, constraint, fp ).reshape( full_image.shape )
+    filt = fpf.FPF( mat_images, constraint, fp ).reshape( full_image.shape )
   else:
-    filt = ifft2( fpf.FPF( mat_boats, constraint, fp ).reshape( full_image.shape ) )
+    filt = ifft2( fpf.FPF( mat_images, constraint, fp ).reshape( full_image.shape ) )
 
+  # perform the correlation
   corr = sophia.Correlate( full_image, filt )
 
+  # show the resulting image
   sophia.a2i( corr.real ).show()
 
-  return filt, corr, centered_boats
+  return filt, corr, centered_images
 
-def loadBoats( num, prefix, extension ):
+def countPeaks( corr, thresh ):
+  peaks = ( corr > thresh ) * buildBorderMask( )
+  peaks = blurMask( peaks, 5 )
+  sophia.a2i( peaks ).show( )
+  labels, count = label( peaks, structure=ones( (3,3) ) )
+  print "Boats:", count
+
+def loadImages( num, prefix, extension ):
+  """Load a series of images with similar names and extensions."""
+
   boats = []
   
   for i in xrange( num ):
@@ -46,8 +93,21 @@ def loadBoats( num, prefix, extension ):
   return boats
 
 def loadMatrix( name ):
+  """
+  Load a single image, converting it to black and white, converting
+  its data type to float, and scaling it to 0 to 1
+  """
   image = Image.open( findfile( name ) ).convert( 'L' )
   return sophia.i2a( image ).astype( float ) / 255
+
+def buildBorderMask( name="boatsmall.jpg" ):
+  return 1.0 - blurMask( loadMatrix( name ) < 0.01, 30 )
+
+def blurMask( mask, amount ):
+  kernel = numpy.ones(amount*amount,float)
+  kernel = kernel / numpy.sum( kernel )
+  kernel = kernel.reshape( (amount,amount) )
+  return convolve2d( mask, kernel, mode="same" ) != 0.0;
 
 # inspired by: http://dotnot.org/blog/archives/2004/03/06/find-a-file-in-pythons-path/
 # I wanted a way to automatically find the named image file in the same way python finds imported modules (similar to seaching for resources on the Java classpath)
@@ -58,4 +118,4 @@ def findfile( name ):
       return potential
 
 if __name__ == "__main__":
-  main()
+  main_allboats()
